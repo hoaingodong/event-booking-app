86245911c394bf4a224cabadf50f82ed907661a2
hoaingodong <hoaingodong1@gmail.com>
2023-06-20 08:32:00 +0700

    fix: rename the api

--- src/routes/event.route.js
+++ src/routes/event.route.js
@@ -5,14 +5,13 @@ const filterSchema = require("../validation/filter.validation")
 const {celebrate, Segments} = require("celebrate");
 const Joi = require("joi");
 const locationSchema = require('../validation/location.validation')
+const joinedEventController = require("../controllers/joinedEvent.controller");
 
-router.get("/", eventController.getAll)
-router.get("/detail/:id", eventController.getDetail)
-router.get("/filter", celebrate({[Segments.QUERY]:filterSchema}), eventController.filter)
-router.get("/filter-location", celebrate({[Segments.QUERY]:locationSchema}),  eventController.filterLocation)
+router.get("/", celebrate({[Segments.QUERY]:filterSchema}), eventController.filter)
+router.get("/nearby", celebrate({[Segments.QUERY]:locationSchema}),  eventController.filterLocation)
 router.get("/search", celebrate({[Segments.QUERY]: {keyword: Joi.string()}}), eventController.search)
-
-
+router.get("/:id", eventController.getDetail)
+router.get("/:id/users", joinedEventController.getAllUsers)
 
 module.exports = router
 
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,4 @@
 const express = require("express")
-const router = express.Router()
 const userController = require("../controllers/users.controller")
 const userSchema = require("../validation/user.validation")
 const otpSchema = require("../validation/otp.validation")
@@ -8,13 +7,16 @@ const Joi = require("joi");
 const resetSchema = require("../validation/resetPassword.validation")
 const notificationController = require("../controllers/inviteFriends.controller")
 const joinedEventController = require("../controllers/joinedEvent.controller")
-const {celebrate, Segments} = require("celebrate");
 const joinEventSchema = require("../validation/joinEvent.validation")
+const profileController = require("../controllers/profile.controller");
+const router = express.Router()
+const upload = require("../config/multer.config")
+const profileSchema = require("../validation/profile.validation")
+const {celebrate, Segments} = require("celebrate");
 
 router.post("/register", celebrate({[Segments.BODY]:userSchema}), userController.createNew)
 router.post("/verify-otp", celebrate({[Segments.BODY]:otpSchema}), userController.verifyOTP)
 router.post("/login", celebrate({[Segments.BODY]:loginSchema}), userController.login)
-//forgot password - nhập vào email thui (kiểm tra đã verified otp đợt đầu hay chưa) - gửi otp cho email - nhập otp - nhập password, confirmpassword ->ok
 router.post("/send-email-reset-password", celebrate({[Segments.BODY]: {email: Joi.string().email().required()}}), userController.sendEmailResetPassword)
 router.post("/reset-password", celebrate({[Segments.BODY]:resetSchema}), userController.resetPassword)
 router.get("/", userController.getAll)
@@ -25,8 +27,14 @@ router.post("/join-event", celebrate({[Segments.BODY]:joinEventSchema}), joinedE
 router.get("/:id/events", joinedEventController.getAllEvents)
 router.get("/:id/upcoming-events", joinedEventController.getUpcomingEvent)
 router.get("/:id/last-events", joinedEventController.getLastEvent)
-
-
+//profile
+router.post("/upload-avatar/:id", upload.upload.any(), profileController.uploadAvatar)
+router.post("/edit-avatar/:id", upload.upload.any(), profileController.editAvatar)
+router.delete("/delete-avatar/:id", profileController.deleteAvatar)
+router.get("/profile-event/:id", profileController.profileEvent)
+router.get("/profile-about/:id", profileController.profileAbout)
+router.get("/profile-reviews/:id", profileController.profileReviews)
+router.put("/:id", celebrate({[Segments.BODY]:profileSchema}), profileController.editProfile)
 
 module.exports = router
 
