79378291446b8355a8d370e6b50531e86de49feb
hoaingodong <hoaingodong1@gmail.com>
2023-06-20 08:32:00 +0700

    fix: rename the api

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -2,10 +2,8 @@ const express = require("express")
 const router = express.Router()
 const usersRouter = require("./user.route")
 const eventRouter = require("./event.route")
-const profileRouter = require("./profile.route")
 
 router.use("/users", usersRouter)
 router.use("/events", eventRouter)
-router.use("/profile", profileRouter)
 
 module.exports = router
