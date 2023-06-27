db8e03cd82552b6dd0056867cbdfbe88ea2d0171
hoaingodong <hoaingodong1@gmail.com>
2023-06-27 14:21:51 +0700

    fix: rename variables and reformat code of joined table

--- src/services/joinedEvent.service.js
+++ src/services/joinedEvent.service.js
@@ -2,72 +2,72 @@ const JoinedEvent = require("../models/joinedEvent.model")
 const User = require("../models/user.model")
 const Event = require("../models/event.model")
 
-const createNew = async (user_id, event_id) => {
+const createNew = async (userId, eventId) => {
 
-    const user = await User.findById(user_id)
+    const user = await User.findById(userId)
     if (!user){
         throw new Error("User not found")
     }
-    const event = await Event.findById(event_id)
+    const event = await Event.findById(eventId)
     if (!event){
         throw new Error("Event not found")
     }
 
     const myEvent = {
-        user: user_id,
-        event: event_id
+        user: userId,
+        event: eventId
     }
 
     const savedMyEvent = await JoinedEvent.create({...myEvent})
     return savedMyEvent
 }
 
-const getAllEvents = async (user_id) => {
+const getAllEvents = async (userId) => {
 
-    const user = await User.findById(user_id)
+    const user = await User.findById(userId)
     if (!user){
         throw new Error("User not found")
     }
 
-    const myEvents = await JoinedEvent.find({user: user_id}).populate("event")
+    const myEvents = await JoinedEvent.find({user: userId}).populate("event")
     return myEvents
 }
 
-const getUpcomingEvent = async (user_id) => {
+const getUpcomingEvent = async (userId) => {
 
-    const user = await User.findById(user_id)
+    const user = await User.findById(userId)
     if (!user){
         throw new Error("User not found")
     }
-    const myEvents = await JoinedEvent.find({user: user_id}).populate("event")
+    const myEvents = await JoinedEvent.find({user: userId}).populate("event")
 
-    const myUpcomingEvents = await myEvents.filter(element => element.event.started_date > Date.now())
+    const myUpcomingEvents = await myEvents.filter(element => element.event.startDate > Date.now())
 
     return myUpcomingEvents
 }
 
-const getLastEvent = async (user_id) => {
+const getLastEvent = async (userId) => {
 
-    const user = await User.findById(user_id)
+    const user = await User.findById(userId)
     if (!user){
         throw new Error("User not found")
     }
-    const myEvents = await JoinedEvent.find({user: user_id}).populate("event")
+    const myEvents = await JoinedEvent.find({user: userId}).populate("event")
 
-    const myLastEvents = await myEvents.filter(element => element.event.started_date < Date.now())
+    const myLastEvents = await myEvents.filter(element => element.event.startDate < Date.now())
 
     return myLastEvents
 }
 
-const getAllUsers = async (event_id) => {
+const getAllUsers = async (eventId) => {
 
-    const event = await Event.findById(event_id)
+    const event = await Event.findById(eventId)
     if (!event){
         throw new Error("Event not found")
     }
 
-    const users = await JoinedEvent.find({event: event_id}).populate("user")
-    const totalUser = await JoinedEvent.find({event: event_id}).populate("user").count()
+    const users = await JoinedEvent.find({event: eventId}).populate("user")
+    const totalUser = await JoinedEvent.find({event: eventId}).populate("user").count()
 
     return {users, totalUser}
 }
