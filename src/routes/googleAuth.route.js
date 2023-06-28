e0cbb0a26a84befe68a12b90c15a818d4fb9f0f7
hoaingodong <hoaingodong1@gmail.com>
2023-06-29 00:24:20 +0700

    fix: fix handle google passport

--- src/routes/googleAuth.route.js
+++ src/routes/googleAuth.route.js
@@ -18,23 +18,21 @@ passport.use(new GoogleTokenStrategy({
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
     },
-     async function (accessToken, refreshToken, profile, done) {
+    async function (accessToken, refreshToken, profile, done) {
         console.log(accessToken, refreshToken, profile, done)
 
-        const email = profile.emails[0].value;
-        const name = profile.displayName
-
         User.findOrCreate({ email: profile.emails[0].value}, function (err, user) {
+            console.log(user)
             return done(err, user);
           });
         }
-));
+)
+);
 router.post('/google/token', passport.authenticate('google-token'),
     async function(req, res) {
-    // res.send(req.user);
     const user = req.user
     const token = await user.getJwtToken()
-    return res.status(200).json({token})
+    return res.status(200).json({token, user})
 });
 
 module.exports = router
