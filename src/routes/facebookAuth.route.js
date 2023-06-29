5f8b3db5944e9ce294c266029dae2d7abf2febd9
hoaingodong <hoaingodong1@gmail.com>
2023-06-29 10:35:07 +0700

    fix handle facebook authentication function

--- src/routes/facebookAuth.route.js
+++ src/routes/facebookAuth.route.js
@@ -6,35 +6,36 @@ const FacebookTokenStrategy = require('passport-facebook-token');
 const User = require("../models/user.model")
 const userService = require("../services/user.service")
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
+passport.serializeUser(function (user, done) {
+    done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
-  done(null, user);
+passport.deserializeUser(function (user, done) {
+    done(null, user);
 });
 
 passport.use(new FacebookTokenStrategy({
-    clientID: process.env.FACEBOOK_APP_ID,
-    clientSecret: process.env.FACEBOOK_APP_SECRET,
-    fbGraphVersion: 'v3.0'
-  }, async function (accessToken, refreshToken, profile, done) {
-            console.log(accessToken, refreshToken, profile, done)
-            User.findOrCreate({facebookId: profile.id}, function (err, user) {
-              user.name = profile.displayName
-              user.avatar = {url: profile._json.picture}
-              user.save()
-              return done(err, user);
-              });
-            }
+        clientID: process.env.FACEBOOK_APP_ID,
+        clientSecret: process.env.FACEBOOK_APP_SECRET,
+        fbGraphVersion: 'v3.0'
+    }, async function (accessToken, refreshToken, profile, done) {
+        console.log(accessToken, refreshToken, profile, done)
+        User.findOrCreate({facebookId: profile.id}, function (err, user) {
+            user.name = profile.displayName
+            user.email = profile.emails[0].value
+            user.avatar = {url: profile.photos[0].value}
+            user.save()
+            return done(err, user);
+        });
+    }
 ));
 
-router.post('/facebook/token', passport.authenticate('facebook-token'), 
-   async function(req, res) {
-    const user = req.user
-    const token = await user.getJwtToken()
-    return res.status(200).json({token, user})
-});
+router.post('/facebook/token', passport.authenticate('facebook-token'),
+    async function (req, res) {
+        const user = req.user
+        const token = await user.getJwtToken()
+        return res.status(200).json({token, user})
+    });
 
 module.exports = router
 
