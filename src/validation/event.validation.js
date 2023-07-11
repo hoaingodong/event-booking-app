d85b343aa7731a9027998c3f0003700e510bb1d3
hoaingodong <hoaingodong1@gmail.com>
2023-07-11 10:59:46 +0700

    feat: add location, image validation of update event

--- src/validation/event.validation.js
+++ src/validation/event.validation.js
@@ -16,7 +16,9 @@ const updateEventSchema = Joi.object().keys({
         Joi.number().required(),
         Joi.string().required()
     ],
-    address: Joi.string().required()
+    address: Joi.string().required(),
+    location: Joi.any(),
+    image: Joi.any()
 })
 
 const createEventSchema = updateEventSchema.keys({
